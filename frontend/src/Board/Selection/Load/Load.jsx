e7eb63979434521a25dd6ef52ab7fb66cca4e285
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2024-12-09 02:04:46 -0500

    Reset

--- frontend/src/Board/Selection/Load/Load.jsx
+++
@@ -1,8 +0,0 @@
-
-function Load(props){
-    return (
-        <button id="load_button" className="button" onClick={props.deleteNote}></button>
-    )
-}
-
-export default Load
--- frontend/src/Board/Selection/Store/Store.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Store(props){
-    return(
-        <button id="store_button" className="button" onClick={props.storeNote}></button>
-    )
-}
-
-export default Store
